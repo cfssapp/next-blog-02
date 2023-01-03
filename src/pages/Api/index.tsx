79999154ff7b97bbbc168945da4a691fc91ac25c
cfssapp <gohweihan21@gmail.com>
2023-01-03 19:13:17 +0800

    new commit

--- src/pages/Api/index.tsx
+++ src/pages/Api/index.tsx
@@ -22,20 +22,28 @@ export async function getStaticProps() {
   };
 }
 
-const HomePage: React.FC = () => {
-  const { name } = useModel('global');
-  return (
-    <PageContainer ghost>
-      <Layout>
-        <Row>
-          <Typography.Title level={3} className={styles.title}>
-            Testing <strong>API</strong> ！
-          </Typography.Title>
-          
-        </Row>
-      </Layout>
-    </PageContainer>
-  );
-};
+export default ({posts}) => (
+  <ul> 
+   {posts.map(post => (
+     <li key={post.id}>{post.title}</li>
+    ))}
+  </ul>
+);
 
-export default HomePage;
+// const HomePage: React.FC = () => {
+//   const { name } = useModel('global');
+//   return (
+//     <PageContainer ghost>
+//       <Layout>
+//         <Row>
+//           <Typography.Title level={3} className={styles.title}>
+//             Testing <strong>API</strong> ！
+//           </Typography.Title>
+          
+//         </Row>
+//       </Layout>
+//     </PageContainer>
+//   );
+// };
+
+// export default HomePage;
