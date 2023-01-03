c36aca565c6feb9a51045b3d75c2ed792a7e561b
cfssapp <gohweihan21@gmail.com>
2023-01-03 18:45:34 +0800

    new commit

--- src/pages/Api/index.tsx
+++ src/pages/Api/index.tsx
@@ -1,16 +1,31 @@
+import { Layout, Row, Typography } from 'antd';
 import Guide from '@/components/Guide';
 import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
 import styles from './index.less';
 
+import { request } from '@umijs/max';
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+prisma = new PrismaClient();
+const allPosts = prisma.post.findMany({ });
+console.log(allPosts);
+
+
+
 const HomePage: React.FC = () => {
   const { name } = useModel('global');
   return (
     <PageContainer ghost>
-      <div className={styles.container}>
-        <Guide name={trim(name)} />
-      </div>
+      <Layout>
+      <Row>
+        <Typography.Title level={3} className={styles.title}>
+          欢迎使用 <strong>{name}</strong> ！
+        </Typography.Title>
+      </Row>
+    </Layout>
     </PageContainer>
   );
 };
