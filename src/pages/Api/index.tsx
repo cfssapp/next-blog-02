3f80469eb8f219650509cf9fad94d01741ad607a
cfssapp <gohweihan21@gmail.com>
2023-01-03 18:58:58 +0800

    new commit

--- src/pages/Api/index.tsx
+++ src/pages/Api/index.tsx
@@ -5,34 +5,34 @@ import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
 import styles from './index.less';
 
-import { request } from '@umijs/max';
-import { PrismaClient } from '@prisma/client';
+// import { request } from '@umijs/max';
+// import { PrismaClient } from '@prisma/client';
 
-export async function getStaticProps() {
-  const prisma = new PrismaClient()
-  const allposts = await prisma.post.findMany()
+// export async function getStaticProps() {
+//   const prisma = new PrismaClient()
+//   const allposts = await prisma.post.findMany()
 
-  console.log(allposts);
-  return {
-    props : { allposts }
-  }
-}
+//   console.log(allposts);
+//   return {
+//     props : { allposts }
+//   }
+// }
 
 
 
-// const HomePage: React.FC = () => {
-//   const { name } = useModel('global');
-//   return (
-//     <PageContainer ghost>
-//       <Layout>
-//       <Row>
-//         <Typography.Title level={3} className={styles.title}>
-//           Testing <strong>API</strong> ！
-//         </Typography.Title>
-//       </Row>
-//     </Layout>
-//     </PageContainer>
-//   );
-// };
+const HomePage: React.FC = () => {
+  const { name } = useModel('global');
+  return (
+    <PageContainer ghost>
+      <Layout>
+      <Row>
+        <Typography.Title level={3} className={styles.title}>
+          Testing <strong>API</strong> ！
+        </Typography.Title>
+      </Row>
+    </Layout>
+    </PageContainer>
+  );
+};
 
-// export default HomePage;
+export default HomePage;
